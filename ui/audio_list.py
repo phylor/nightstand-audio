b2a10f4e5b1813a62b430098229e5a4c62e17e45
Serge Hänni <serge.haenni@simplificator.com>
2017-11-12 01:24:27 +0100

    fixed loading of json

--- ui/audio_list.py
+++ ui/audio_list.py
@@ -33,7 +33,8 @@ class AudioList(RecycleView):
 
         for json_file in glob.glob(os.path.join(self.data_directory, 'figurines', '*.json')):
             with open(json_file, 'r') as json_data:
-                files.append(json.loads(json_data).audio_path)
+                data = json_data.read()
+                files.append(json.loads(data).audio_path)
 
         return files
 
